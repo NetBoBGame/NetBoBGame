3a0709f3a8e696d9391696713c85714c4512ebe4
savanaKahn <kahn@savanaKahnui-MacBookPro.local>
2020-08-22 00:09:30 +0900

    made Net

---
+++ GameManager.cs
@@ -0,0 +1,81 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null) instance = FindObjectOfType<GameManager>();
+
+            return instance;
+        }
+    }
+
+    private static GameManager instance;
+
+    public Text scoreText;
+    public Transform[] spawnPositions;
+    public GameObject playerPrefab;
+    public GameObject ballPrefab;
+
+    private int[] playerScores;
+
+    private void Start()
+    {
+        playerScores = new[] {0,0};
+        //each Player activation each 1 time.
+        SpawnPlayer();
+        if(PhotonNetwork.IsMasterClient)
+        {
+            SpawnBall();
+        }
+    }
+
+    private void SpawnPlayer()
+    {
+        //inside we handle character 0, 1
+        var localPlayerIndex = PhotonNetwork.LocalPlayer.ActorNumber -1 ;
+        var spawnPosition = spawnPositions[localPlayerIndex % spawnPositions.Length];
+
+        PhotonNetwork.Instantiate(playerPrefab.name,spawnPosition.position,spawnPosition.rotation);
+    }
+
+    private void SpawnBall()
+    {
+        PhotonNetwork.Instantiate(ballPrefab.name,Vector2.zero, Quaternion.identity);
+    }
+
+    public override void OnLeftRoom()
+    {
+        //one person is left except others.
+        SceneManager.LoadScene("Lobby");
+    }
+    public void Leave()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+    public void AddScore(int playerNumber, int score)
+    {
+        //only master update score.
+        if(!PhotonNetwork.IsMasterClient)
+        {
+            return;
+        }
+        playerScores[playerNumber -1 ] += score;
+
+        //everybody shouting.
+        photonView.RPC("RPCUpdateScoreText", RpcTarget.All,playerScores[0].ToString(),playerScores[1].ToString());
+    }
+
+
+    
+    [PunRPC]
+    private void RPCUpdateScoreText(string player1ScoreText, string player2ScoreText)
+    {
+        scoreText.text = $"{player1ScoreText} : {player2ScoreText}";
+    }
+}
---
+++ LobbyManager.cs
@@ -0,0 +1,59 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    private readonly string gameVersion = "1";
+
+    public Text connectionInfoText;
+    public Button joinButton;
+    
+    private void Start()
+    {
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+
+        joinButton.interactable = false;
+        connectionInfoText.text = "Connecting To Master Server...";
+    }
+    
+    public override void OnConnectedToMaster()
+    {
+        joinButton.interactable = true;
+        connectionInfoText.text = "Online : Connected to Master Server";
+    }
+    
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        joinButton.interactable = false;
+        connectionInfoText.text = $"Offline : Connection Disabled {cause.ToString()} ";
+    }
+    
+    public void Connect()
+    {
+        joinButton.interactable = false;
+
+        if(PhotonNetwork.IsConnected)
+        {
+            connectionInfoText.text = "Connecting to Random Room...";
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else{
+            connectionInfoText.text = "Offline : Connection Disabled - Try reconnect";
+        }
+    }
+    
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        connectionInfoText.text = "there is no empty room, creating new Room";
+        PhotonNetwork.CreateRoom(null,new RoomOptions { MaxPlayers = 2});
+    }
+    
+    public override void OnJoinedRoom()
+    {
+        connectionInfoText.text = "Connected with Room";
+        //anybody in this room all together move to Game
+        PhotonNetwork.LoadLevel("Main");
+    }
+}
