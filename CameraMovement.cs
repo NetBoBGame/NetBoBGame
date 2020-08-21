6dac9061283324d92a488b0dbf84b4bf2f811554
savanaKahn <kahn@savanaKahnui-MacBookPro.local>
2020-08-22 03:15:49 +0900

    Add 2

---
+++ CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject character;
+    public float offsetX;
+    public float offsetY;
+    public float offsetZ;
+
+    Vector3 cameraPosition ; 
+    // Start is called before the first frame update
+    private void LateUpdate() {
+        cameraPosition.x = character.transform.position.x + offsetX;
+        cameraPosition.y = character.transform.position.y + offsetY;
+        cameraPosition.z = character.transform.position.z + offsetZ;
+
+        transform.position = cameraPosition;
+    }
+}
---
+++ CharacterController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private float h = 0f;
+    private float v = 0f;
+    private float r = 0f;
+
+    private Transform tr;
+    
+    private float moveSpeed = 5f;
+    public float rotSpeed = 50f;
+
+    private bool runSwitch;
+    private bool jumpSwitch;
+
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        runSwitch = false;
+        
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        r = Input.GetAxis("Mouse X");
+
+        if(h != 0 || v !=0)
+        {
+            runSwitch = true;
+        }
+        else{
+            runSwitch = false;
+            animator.SetBool("run",false);
+        }
+        if(runSwitch)
+        {
+            animator.SetBool("run", true);
+            Vector3 moveDir = (Vector3.forward * v) + (Vector3.right *h);
+            tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+            tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * h);
+        }
+       
+    }
+}
