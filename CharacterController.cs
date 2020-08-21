467f26982be1271eb51e960ab7ed4429528d8a71
savanaKahn <kahn@savanaKahnui-MacBookPro.local>
2020-08-22 04:50:28 +0900

    edit Character Controller

--- CharacterController.cs
+++ CharacterController.cs
@@ -8,18 +8,23 @@ public class CharacterController : MonoBehaviour
     private float v = 0f;
     private float r = 0f;
 
+    private Animator animator;
     private Transform tr;
+    private Rigidbody rigidbody;
     
     private float moveSpeed = 5f;
     public float rotSpeed = 50f;
+    public float jumpPower = 10f;
 
     private bool runSwitch;
     private bool jumpSwitch;
 
-    private Animator animator;
+    private Vector3 movement;
+    
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody  = GetComponent<Rigidbody>();
         tr = GetComponent<Transform>();
         animator = GetComponent<Animator>();
     }
@@ -31,23 +36,42 @@ public class CharacterController : MonoBehaviour
         
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
-        r = Input.GetAxis("Mouse X");
-
+        
+        if(Input.GetButtonDown("Jump"))
+        {
+            jumpSwitch = true;
+            Jump();
+        }
+        
+        
         if(h != 0 || v !=0)
         {
             runSwitch = true;
+            Move(h,v);
         }
         else{
             runSwitch = false;
             animator.SetBool("run",false);
         }
-        if(runSwitch)
-        {
-            animator.SetBool("run", true);
-            Vector3 moveDir = (Vector3.forward * v) + (Vector3.right *h);
-            tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
-            tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * h);
-        }
-       
     }
+    private void Move(float h, float v) {
+        animator.SetBool("run", true);
+        movement.Set (h,0,v );
+        movement = movement.normalized * moveSpeed * Time.deltaTime;
+        rigidbody.MovePosition (transform.position + movement);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+    }
+    private void Jump()
+    {
+        if(!jumpSwitch)
+        {
+            return;
+        }
+        animator.SetBool("jump",true);
+        rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+
+        
+        jumpSwitch = false;
+    }
+    
 }
